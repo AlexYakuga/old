15a30616cca8612d92b7c886021d276361367824
AlexYakuga <alexyakuga@gmail.com>
2021-02-03 21:27:48 +0300

    new

--- fulscreen-menu.js
+++ fulscreen-menu.js
@@ -1,6 +1,12 @@
-const button = document.querySelector('#open')
 const menu = document.querySelector('#menu')
+const button = document.querySelector('#open')
 
-button.addEventListener('click', () => {
-  menu.classList.toggle('menu--active')
-})
+const close = document.querySelector('#close')
+
+const clickFn = (e) => {
+  e.preventDefault()
+  menu.classList.toggle('fullscreen-menu--active')
+}
+
+button.addEventListener('click', clickFn)
+close.addEventListener('click', clickFn)
